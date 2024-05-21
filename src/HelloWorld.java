303691595ff58a6fcb7c74f817f50575f69cbc4f
CMPierce19 <cmpierce19@gmail.com>
2024-05-21 10:43:10 -0400

    ProgrammerOne changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Cameron is so cooler!");
-		System.out.println("Cameron is no longer cool.");
+		System.out.println("Programmer1 is so cooler!");
+		System.out.println("Programmer1 is no longer cool.");
 		System.out.println("Programmer2 did this and is way better.");
 	}
 }
